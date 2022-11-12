70031f4ad366a12ca35d5dc467df91bbdbe6ea3d
itsme-sashank <82751140+itsme-sashank@users.noreply.github.com>
2022-11-12 13:43:08 +0530

    updatedFlutter

--- socialme/ios/Flutter/flutter_export_environment.sh
+++ socialme/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\DS204Project\socialme"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sashank lappy\Documents\GitHub\DS204Project\socialme"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
